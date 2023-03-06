ecb1c4c58caac5c117f5fe7f14233b30d0f18bad
Ashakuur1 <engshakrayare114@gmail.com>
2023-03-05 20:15:36 -0800

    first php init

---
+++ arrays/index.php
@@ -0,0 +1,14 @@
+<?php
+//Array waa loogu talagalay multible values inuu haayo
+
+$Array = [12,13,45,12];
+
+// echo $Array[2];
+
+// print_r($Array[2]);
+
+
+// arrays method
+echo count($Array)
+
+?>
---
+++ variable/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// integer
+
+$number = 1; 
+$number2 = 10;
+
+$number3 = $number + $number2;
+
+echo $number3;
+
+
+?>
